65cb93eb7bb13938503f690cd8a42c82a7ae90c0
petrubear <edison.martinez@gmail.com>
2023-02-05 12:03:16 -0500

    feat: reintentos al llamar al servicio de catalogos

--- order-service/src/main/java/emg/cloud/bookshop/orderservice/domain/dto/BookClient.java
+++ order-service/src/main/java/emg/cloud/bookshop/orderservice/domain/dto/BookClient.java
@@ -2,7 +2,11 @@ package emg.cloud.bookshop.orderservice.domain.dto;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
+
+import java.time.Duration;
 
 @Component
 public class BookClient {
@@ -17,6 +21,10 @@ public class BookClient {
         return webClient.get()
             .uri(BOOKS_ROOT_API + isbn)
             .retrieve()
-            .bodyToMono(Book.class);
+            .bodyToMono(Book.class)
+            .timeout(Duration.ofSeconds(3), Mono.empty())
+            .onErrorResume(WebClientResponseException.NotFound.class, exception -> Mono.empty())
+            .retryWhen(Retry.backoff(3, Duration.ofMillis(100)))
+            .onErrorResume(Exception.class, exception -> Mono.empty());
     }
 }

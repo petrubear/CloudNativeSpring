eeee86c8650b6144e0bbe6b9f8c1598308709046
petrubear <edison.martinez@gmail.com>
2023-02-06 19:58:08 -0500

    feat: opciones de resiliencia y sesiones en redis

--- edge-service/src/test/java/emg/cloud/bookshop/edgeservice/EdgeServiceApplicationTests.java
+++ edge-service/src/test/java/emg/cloud/bookshop/edgeservice/EdgeServiceApplicationTests.java
@@ -2,12 +2,34 @@ package emg.cloud.bookshop.edgeservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+@SpringBootTest(
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
+@Testcontainers
 class EdgeServiceApplicationTests {
+    private static final int REDIS_PORT = 6379;
 
-	@Test
-	void contextLoads() {
-	}
+    @Container
+    static GenericContainer<?> redis = new GenericContainer<>("redis:7.0")
+        .withExposedPorts(REDIS_PORT);
+
+
+    @DynamicPropertySource
+    static void redisProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.redis.host",
+            () -> redis.getHost());
+        registry.add("spring.redis.port",
+            () -> redis.getMappedPort(REDIS_PORT));
+    }
+
+    @Test
+    void verifyThatSpringContextLoads() {
+    }
 
 }

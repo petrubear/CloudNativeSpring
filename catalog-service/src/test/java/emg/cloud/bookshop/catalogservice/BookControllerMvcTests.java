c5e356a266bbd87c59ecfe3cfe825df785e4546f
petrubear <edison.martinez@gmail.com>
2023-02-15 19:56:38 -0500

    feat: seguridad en servicios downstream

--- catalog-service/src/test/java/emg/cloud/bookshop/catalogservice/BookControllerMvcTests.java
+++ catalog-service/src/test/java/emg/cloud/bookshop/catalogservice/BookControllerMvcTests.java
@@ -1,5 +1,6 @@
 package emg.cloud.bookshop.catalogservice;
 
+import emg.cloud.bookshop.catalogservice.configuration.SecurityConfig;
 import emg.cloud.bookshop.catalogservice.domain.exceptions.BookNotFoundException;
 import emg.cloud.bookshop.catalogservice.domain.service.BookService;
 import emg.cloud.bookshop.catalogservice.web.BookController;
@@ -7,7 +8,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -15,12 +22,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @WebMvcTest(BookController.class)
+@Import(SecurityConfig.class)
 class BookControllerMvcTests {
     @Autowired
     private MockMvc mockMvc;
 
     @MockBean
     private BookService bookService;
+
+    @MockBean
+    JwtDecoder jwtDecoder;
 
     @Test
     void whenGetBookNotExistingThenShouldReturn404() throws Exception {
@@ -29,5 +40,36 @@ class BookControllerMvcTests {
             .willThrow(BookNotFoundException.class);
         mockMvc.perform(get("/books/{isbn}", isbn))
             .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void whenDeleteBookWithEmployeeRoleThenShouldReturn204()
+        throws Exception {
+        var isbn = "7373731394";
+        mockMvc
+            .perform(MockMvcRequestBuilders.delete("/books/" + isbn)
+                .with(SecurityMockMvcRequestPostProcessors.jwt()
+                    .authorities(new SimpleGrantedAuthority("ROLE_employee"))))
+            .andExpect(MockMvcResultMatchers.status().isNoContent());
+    }
+
+    @Test
+    void whenDeleteBookWithCustomerRoleThenShouldReturn403()
+        throws Exception {
+        var isbn = "7373731394";
+        mockMvc
+            .perform(MockMvcRequestBuilders.delete("/books/" + isbn)
+                .with(SecurityMockMvcRequestPostProcessors.jwt()
+                    .authorities(new SimpleGrantedAuthority("ROLE_customer"))))
+            .andExpect(MockMvcResultMatchers.status().isForbidden());
+    }
+
+    @Test
+    void whenDeleteBookNotAuthenticatedThenShouldReturn401()
+        throws Exception {
+        var isbn = "7373731394";
+        mockMvc
+            .perform(MockMvcRequestBuilders.delete("/books/" + isbn))
+            .andExpect(MockMvcResultMatchers.status().isUnauthorized());
     }
 }
